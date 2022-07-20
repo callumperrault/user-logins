5976afc3bea051dbb24963e4d5c02edd403f5de7
Callum Perrault <callumperrault2@gmail.com>
2022-07-20 18:41:18 -0400

    Updated the project to now store usernames and passwords in a config file rather than a dictionary.

--- user-logins.py
+++ user-logins.py
@@ -1,22 +1,21 @@
 import hashlib
+import yaml
 
-# Dictionary to store usernames and hashed passwords
-usernames_and_passwords = {
-    "hello" : "b8fd23c8ad9f90270d6ab278db7aae63318cb9b1d58922bf711a38d29251263f"
-}
+with open('./usernames-passwords.yaml', 'r') as credentials:
+    usernames_and_passwords = yaml.safe_load(credentials)
+
 
 def is_valid_credentials(username, password):
     # Password is hashed here
     hashed_password = hashlib.sha256(password.encode())
     hashed_password = hashed_password.hexdigest()
     
-    # Username and password are compared to the ones in the dictionary, True is returned if they
+    # Username and password are compared to the ones in the config file, True is returned if they
     # match and false is returned otherwise
-    for key in usernames_and_passwords:
-        if key == username:
-            if usernames_and_passwords[key] == hashed_password:
+    for users in usernames_and_passwords:
+        if users['username'] == username:
+            if users['password_hash'] == hashed_password:
                 return(True)
 
     return(False)
-
 
