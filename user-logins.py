ccb57dcd24b60553bb2f8890f1a4ed2c88c11ca2
Callum Perrault <callumperrault2@gmail.com>
2022-08-06 19:44:20 -0400

    Updated user-logins to integrate with database

--- user-logins.py
+++ user-logins.py
@@ -1,21 +1,39 @@
 import hashlib
-import yaml
+import sqlite3
+from urllib.request import HTTPBasicAuthHandler
 
-with open('./usernames-passwords.yaml', 'r') as credentials:
-    usernames_and_passwords = yaml.safe_load(credentials)
-
+db_name = 'ppab6.db'
 
 def is_valid_credentials(username, password):
+    # Connection is made to database and cursor is created
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
     # Password is hashed here
     hashed_password = hashlib.sha256(password.encode())
     hashed_password = hashed_password.hexdigest()
     
     # Username and password are compared to the ones in the config file, True is returned if they
     # match and false is returned otherwise
-    for users in usernames_and_passwords:
-        if users['username'] == username:
-            if users['password_hash'] == hashed_password:
-                return(True)
+    cursor.execute('''SELECT rowid FROM users WHERE usernames = ?''', (username,))
+    username_ = cursor.fetchone()
 
-    return(False)
+    cursor.execute('''SELECT rowid FROM users WHERE hashed_passwords = ?''', (hashed_password,))
+    password_ = cursor.fetchone()
+    return((username_ is not None) and  (password_ is not None))
 
+while True:
+    
+    username = input("What is your username?\n")
+    password = input("What is your password?\n")
+
+    if is_valid_credentials(username, password):
+        print("Username and password are correct. Granting access now")
+        break
+
+    else:
+        print("Username or password is incorrect. Please try again")
+            
+
+
+        
